3c73ff7b727f886e1fe3b8a3f06d0eddfa294c6a
7oa <semenova.7oa@yandex.ru>
2016-12-26 18:01:58 +0300

    Верстка

---
+++ makeup/js/parallax.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+
+	
+	/* Scroll event handler */
+    $(window).bind('scroll',function(e){
+    	parallaxScroll();
+    });
+    
+	
+    
+});
+
+/* Scroll the background layers */
+function parallaxScroll(){
+	var scrolled = $(window).scrollTop();
+	$('#parallax-bg1').css('top',(0-(scrolled*.5))+'px');
+	$('#parallax-bg2').css('top',(0-(scrolled*.7))+'px');
+	$('#parallax-bg3').css('top',(0-(scrolled*.9))+'px');
+	
+}
+
+
